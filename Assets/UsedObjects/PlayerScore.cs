5b41a4b47dc977be6c79bf553899dbb4cf7cc70c
SamC <samcipolat@gmail.com>
2017-02-14 13:07:12 +0000

    Added class to control player score
    Started simple interface to control the score of the player

---
+++ Assets/UsedObjects/PlayerScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+    int m_Score;
+
+	void Start()
+	{
+		m_Score = 0;
+	}
+	
+	//Amount to add is optional, there incase a double points system
+    public void AddScore(int p_ScoreToAdd = 1)
+    {
+        m_Score += p_ScoreToAdd;
+    }
+
+    public void ResetScore()
+    {
+        m_Score = 0;
+    }
+
+	public string SerializeScore()
+	{
+		return "PScore:" + Convert.ToString(m_Score);
+	}
+	
+}
